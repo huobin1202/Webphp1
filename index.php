67b77f8572ec65388c995d07dae2ec35d4ad9a9e
vphatnguyen <vanphat2k5@gmail.com>
2025-03-04 09:16:39 +0700

    tét

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $username = isset($_SESSION["username"]) ? $_SESSION["username"] : null;
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+<head> hello
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
