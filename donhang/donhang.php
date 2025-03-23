dc11759cacf04607e589c2225d5683de08bc9ae0
huobin1202 <anvabinh123@gmail.com>
2025-03-23 13:01:10 +0700

    hehe

--- donhang/donhang.php
+++ donhang/donhang.php
@@ -1,5 +1,7 @@
-<!DOCTYPE php>
-<php lang="en">
+
+
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -21,76 +23,76 @@
         </button>
     </header>
     <div class="container">
-    <aside class="sidebar open">
-                <div class="top-sidebar">
-                    <a href="#" class="channel-logo"><img src="../img/logo.png" alt="Channel Logo"></a>
-                    <div class="hidden-sidebar your-channel"><img src=""
-                            style="height: 30px;" alt="">
-                    </div>
+        <aside class="sidebar open">
+            <div class="top-sidebar">
+                <a href="#" class="channel-logo"><img src="../img/logo.png" alt="Channel Logo"></a>
+                <div class="hidden-sidebar your-channel"><img src=""
+                        style="height: 30px;" alt="">
                 </div>
-                <div class="middle-sidebar">
-                    <ul class="sidebar-list">
-                        <li class="sidebar-list-item tab-content ">
-                            <a href="/admin.php" class="sidebar-link">
-                                <div class="sidebar-icon"><i class="fa-light fa-house"></i></div>
-                                <div class="hidden-sidebar">Trang tổng quan</div>
-                            </a>
-                        </li>
-                        <li class="sidebar-list-item tab-content">
-                            <a href="/sanpham/sanpham.php" class="sidebar-link">
-                                <div class="sidebar-icon"><i class="fa-thin fa-motorcycle"></i></div>
-                                <div class="hidden-sidebar">Sản phẩm</div>
-                            </a>
-                        </li>
-                        <li class="sidebar-list-item tab-content ">
-                            <a href="/khachhang/khachhang.php" class="sidebar-link">
-                                <div class="sidebar-icon"><i class="fa-light fa-users"></i></div>
-                                <div class="hidden-sidebar">Khách hàng</div>
-                            </a>
-                        </li>
-                        <li class="sidebar-list-item tab-content active">
-                            <a href="/donhang/donhang.php" class="sidebar-link">
-                                <div class="sidebar-icon"><i class="fa-light fa-basket-shopping"></i></div>
-                                <div class="hidden-sidebar">Đơn hàng</div>
-                            </a>
-                        </li>
-                        <li class="sidebar-list-item tab-content">
-                            <a href="/thongkesp/thongke.php" class="sidebar-link">
-                                <div class="sidebar-icon"><i class="fa-light fa-chart-simple"></i></div>
-                                <div class="hidden-sidebar">Thống kê sản phẩm</div>
-                            </a>
-                        </li>
-                        <li class="sidebar-list-item tab-content">
-                            <a href="/thongkekh/thongkekh.php" class="sidebar-link">
-                                <div class="sidebar-icon"><i class="fa-light fa-chart-simple"></i></div>
-                                <div class="hidden-sidebar">Thống kê khách hàng</div>
-                            </a>
-                        </li>
+            </div>
+            <div class="middle-sidebar">
+                <ul class="sidebar-list">
+                    <li class="sidebar-list-item tab-content ">
+                        <a href="/admin.php" class="sidebar-link">
+                            <div class="sidebar-icon"><i class="fa-light fa-house"></i></div>
+                            <div class="hidden-sidebar">Trang tổng quan</div>
+                        </a>
+                    </li>
+                    <li class="sidebar-list-item tab-content">
+                        <a href="/sanpham/sanpham.php" class="sidebar-link">
+                            <div class="sidebar-icon"><i class="fa-thin fa-motorcycle"></i></div>
+                            <div class="hidden-sidebar">Sản phẩm</div>
+                        </a>
+                    </li>
+                    <li class="sidebar-list-item tab-content ">
+                        <a href="/khachhang/khachhang.php" class="sidebar-link">
+                            <div class="sidebar-icon"><i class="fa-light fa-users"></i></div>
+                            <div class="hidden-sidebar">Khách hàng</div>
+                        </a>
+                    </li>
+                    <li class="sidebar-list-item tab-content active">
+                        <a href="/donhang/donhang.php" class="sidebar-link">
+                            <div class="sidebar-icon"><i class="fa-light fa-basket-shopping"></i></div>
+                            <div class="hidden-sidebar">Đơn hàng</div>
+                        </a>
+                    </li>
+                    <li class="sidebar-list-item tab-content">
+                        <a href="/thongkesp/thongke.php" class="sidebar-link">
+                            <div class="sidebar-icon"><i class="fa-light fa-chart-simple"></i></div>
+                            <div class="hidden-sidebar">Thống kê sản phẩm</div>
+                        </a>
+                    </li>
+                    <li class="sidebar-list-item tab-content">
+                        <a href="/thongkekh/thongkekh.php" class="sidebar-link">
+                            <div class="sidebar-icon"><i class="fa-light fa-chart-simple"></i></div>
+                            <div class="hidden-sidebar">Thống kê khách hàng</div>
+                        </a>
+                    </li>
 
-                        <div class="spacer" style="height:50px;width:1px"></div>
-                        <li class="sidebar-list-item user-logout" style="border-top: 2px solid rgba(0,0,0,0.12);">
-                            <a href="index.php" class="sidebar-link">
-                                <div class="sidebar-icon"><i class="fa-thin fa-circle-chevron-left"></i></div>
-                                <div class="hidden-sidebar">Trang chủ</div>
-                            </a>
-                        </li>
+                    <div class="spacer" style="height:50px;width:1px"></div>
+                    <li class="sidebar-list-item user-logout" style="border-top: 2px solid rgba(0,0,0,0.12);">
+                        <a href="index.php" class="sidebar-link">
+                            <div class="sidebar-icon"><i class="fa-thin fa-circle-chevron-left"></i></div>
+                            <div class="hidden-sidebar">Trang chủ</div>
+                        </a>
+                    </li>
 
-                        <li class="sidebar-list-item user-logout">
-                            <a href="#" class="sidebar-link">
-                                <div class="sidebar-icon"><i class="fa-light fa-circle-user"></i></div>
-                                <div class="hidden-sidebar" id="name-acc">Admin</div>
-                            </a>
-                        </li>
-                        <li class="sidebar-list-item user-logout">
-                            <a href="#" class="sidebar-link">
-                                <div class="sidebar-icon"><i class="fa-light fa-arrow-right-from-bracket"></i></div>
-                                <div class="hidden-sidebar" id="logoutacc">Đăng xuất</div>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+                    <li class="sidebar-list-item user-logout">
+                        <a href="#" class="sidebar-link">
+                            <div class="sidebar-icon"><i class="fa-light fa-circle-user"></i></div>
+                            <div class="hidden-sidebar" id="name-acc">Admin</div>
+                        </a>
+                    </li>
+                    <li class="sidebar-list-item user-logout">
+                        <a href="#" class="sidebar-link">
+                            <div class="sidebar-icon"><i class="fa-light fa-arrow-right-from-bracket"></i></div>
+                            <div class="hidden-sidebar" id="logoutacc">Đăng xuất</div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
 
-            </aside>
+        </aside>
         <main class="content">
             <!-- Order  -->
             <div class="section">
@@ -113,11 +115,11 @@
                         <form action="" class="fillter-date">
                             <div>
                                 <label for="time-start">Từ</label>
-                                <input type="date" class="form-control-date" id="time-start" value="2024-02-27">
+                                <input type="date" class="form-control-date" id="time-start" >
                             </div>
                             <div>
                                 <label for="time-end">Đến</label>
-                                <input type="date" class="form-control-date" id="time-end" value="2024-12-30">
+                                <input type="date" class="form-control-date" id="time-end" >
                             </div>
                         </form>
                         <button class="btn-reset-order"><i class="fa-light fa-arrow-rotate-right"></i></button>
@@ -131,27 +133,77 @@
                                 <td>Mã KH</td>
 
                                 <td>Tên khách hàng</td>
-                                <td>Ngày đặt</td>   
+                                <td>Ngày đặt</td>
                                 <td>Tổng tiền</td>
                                 <td>Trạng thái</td>
                                 <td>Thao tác</td>
                             </tr>
                         </thead>
+                        <?php
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "";
+                        $dbname = "admindoan";
+
+                        // Create connection
+                        $conn = new mysqli($servername, $username, $password, $dbname);
+
+                        // Check connection
+                        if ($conn->connect_error) {
+                            die("Kết nối thất bại: " . $conn->connect_error);
+                        }
+
+                        $sql = "SELECT 
+                            orders.id AS order_id, 
+                            orders.customer_id, 
+                            orders.total, 
+                            orders.created_at, 
+                            orders.status, 
+                            customer.name AS customer_name
+                        FROM orders
+                        INNER JOIN customer ON orders.customer_id = customer.id
+                        ORDER BY orders.created_at DESC";
+
+                        $result = $conn->query($sql);
+                        ?>
+
                         <tbody id="showOrder">
-                            <tr>
-                                <td>DH1</td>
-                                <td>KH003</td>
-                                <td>Nhật Tiến</td>
-                                <td>02/06/2024</td>
-                                <td>420.000.000đ</td>
-                                <td><span class="status-complete">Đã xử lý</span></td>
-                                <td class="control">
-                                    <a href="chitietdonhang.php"><button class="btn-detail" id=""><i
-                                                class="fa-regular fa-eye"></i> Chi tiết</button></a>
-                                </td>
-                            </tr>
-                       
+                            <?php
+                            if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {
+                                    $orderId = $row['order_id'];
+                                    $customerId = str_pad($row['customer_id'], 2, '0', STR_PAD_LEFT);
+                                    $customerName = htmlspecialchars($row['customer_name']);
+                                    $createdDate = date('d/m/Y', strtotime($row['created_at']));
+                                    $total = number_format($row['total'], 0, ',', '.') . 'đ';
+                                    $status = $row['status'] == 1
+                                        ? '<span class="status-complete">Đã xử lý</span>'
+                                        : '<span class="status-no-complete">Chưa xử lý</span>';
+                                    $detailLink = "chitietdonhang" . $row['order_id'] . ".php";
+                            ?>
+                                    <tr>
+                                        <td><?= $orderId ?></td>
+                                        <td><?= $customerId ?></td>
+                                        <td><?= $customerName ?></td>
+                                        <td><?= $createdDate ?></td>
+                                        <td><?= $total ?></td>
+                                        <td><?= $status ?></td>
+                                        <td class="control">
+                                            <a href="<?= $detailLink ?>"><button class="btn-detail"><i class="fa-regular fa-eye"></i> Chi tiết</button></a>
+                                        </td>
+                                    </tr>
+                            <?php
+                                }
+                            } else {
+                                echo "<tr><td colspan='7'>Không có đơn hàng nào.</td></tr>";
+                            }
+                            ?>
                         </tbody>
+
+                        <?php
+                        $conn->close();
+                        ?>
+
                     </table>
                 </div>
             </div>
@@ -162,4 +214,4 @@
     <script src="../assets/js/admin.js"></script>
 </body>
 
-</php>
+</html>
