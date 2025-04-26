8f4f5c8b0c877067d8c8881b1e1e31338fe772e4
huobin1202 <anvabinh123@gmail.com>
2025-04-26 20:48:23 +0700

    kk

--- admin.php
+++ admin.php
@@ -5,18 +5,7 @@ if (!isset($_SESSION['username'])) {
     exit();
 }
 
-// Database connection
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "admindoan";
-
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include('database.php');
 
 // Query to count users
 $userCountQuery = "SELECT COUNT(*) as user_count FROM customer";
